780c0e1592948f2fa0022a49e2445999093e5c67
Guido Mintjes <guidomintjes@gmail.com>
2021-10-24 21:42:20 +0200

    h

--- TetrisMonoGame/BlockTest.cs
+++ TetrisMonoGame/BlockTest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TetrisMonoGame {
+﻿namespace TetrisMonoGame {
     public class BlockTest {
         public BlockTest() {
 
